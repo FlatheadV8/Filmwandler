7a65afdb15b70e32bb146df2ceb6a793f1fbc26c
FlatheadV8 <FlatheadV8@users.noreply.github.com>
2019-10-14 13:41:13 +0200

    Update Film2MP4+MKV_WebM.sh
    Fehler behoben

--- Film2MP4+MKV_WebM.sh
+++ Film2MP4+MKV_WebM.sh
@@ -24,7 +24,8 @@
 #------------------------------------------------------------------------------#
 
 
-VERSION="v2019092500"
+#VERSION="v2019092500"
+VERSION="v2019101200"
 
 
 ALLE_OPTIONEN="${@}"
@@ -33,22 +34,33 @@ PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
 STOP="Nein"
 
-AVERZ="$(dirname ${0})"                 # Arbeitsverzeichnis, hier liegen diese Dateien
+AVERZ="$(dirname ${0})"			# Arbeitsverzeichnis, hier liegen diese Dateien
 
+#==============================================================================#
+if [ "x${1}" == x ] ; then
+	echo "${0} OPTIONEN"
+	echo "siehe: ${AVERZ}/Filmwandler.sh -h"
+	echo "${0} -q [Quelle] -z [Ziel]"
+	echo "${0} -q Film.avi -z Film"
+	exit 1
+fi
 #==============================================================================#
 
 while [ "${#}" -ne "0" ]; do
         case "${1}" in
                 -q)
-                        FILMDATEI="${2}"        # Name für die Quelldatei
+                        FILMDATEI="${2}"	# Name für die Quelldatei
+                        shift
                         shift
                         ;;
                 -z)
-                        ZIELPFAD="${2}"         # Name für die Zieldatei
+                        ZIELPFAD="${2}"		# Name für die Zieldatei
+                        shift
                         shift
                         ;;
                 -schnitt)
-                        SCHNITTZEITEN="${2}"    # zum Beispiel zum Werbung entfernen (in Sekunden, Dezimaltrennzeichen ist der Punkt): -schnitt "10-432 520-833 1050-1280"
+                        SCHNITTZEITEN="$(echo "${2}" | sed 's/"//g' | sed "s/'//g")"	# zum Beispiel zum Werbung entfernen (in Sekunden, Dezimaltrennzeichen ist der Punkt): -schnitt "10-432 520-833 1050-1280"
+                        shift
                         shift
                         ;;
                 -u)
@@ -57,14 +69,13 @@ while [ "${#}" -ne "0" ]; do
                         # "0" für die erste Untertitelspur
                         # "1" für die zweite Untertitelspur
                         # "0,1" für die erste und die zweite Untertitelspur
-                        UNTERTITEL="${2}"       # -u 0,1,2,3,4
+                        UNTERTITEL="${2}"	# -u 0,1,2,3,4
+                        shift
                         shift
                         ;;
                 *)
-                        if [ "$(echo "${1}"|egrep '^-')" ] ; then
-                                echo "Der Parameter '${1}' wird nicht unterstützt!"
-                                export STOP="Ja"
-                        fi
+                        echo -n .
+			SONSTIGE_OPTIONEN="${SONSTIGE_OPTIONEN} ${1}"
                         shift
                         ;;
         esac
@@ -83,15 +94,15 @@ SIND_UNTERTITEL_VORHANDEN="$(ffprobe -probesize 9223372036G -analyzeduration 922
 # dieses Format immer
 ENDUNG_1="mp4"
 
-# bevorzugtes Format, leider nur ohne Untertitel möglich
+# bevorzugtes freies Format, leider nur ohne Untertitel möglich
 ENDUNG_2="webm"
 
 ### Untertitel
 if [ "x${SIND_UNTERTITEL_VORHANDEN}" != x ] ; then
-        if [ "${UNTERTITEL}" != "=0" ] ; then
-                # alternatives Format, wenn Untertitel vorhanden sind und nicht abgewählt wurden
-                ENDUNG_2="mkv"
-        fi
+	if [ "${UNTERTITEL}" != "=0" ] ; then
+		# alternatives freies Format, wenn Untertitel vorhanden sind und nicht abgewählt wurden
+		ENDUNG_2="mkv"
+	fi
 fi
 
 
@@ -113,16 +124,24 @@ ZIELNAME="$(echo "${ZIELDATEI}" | rev | sed 's/[ ][ ]*/_/g;s/.*[.]//' | rev)"
 ### Schnitt
 
 if [ "x${SCHNITTZEITEN}" != x ] ; then
-        SCHNITT_OPTION="-schnitt '${SCHNITTZEITEN}'"
+	SCHNITT_OPTION="-schnitt \"${SCHNITTZEITEN}\""
 fi
+
+echo "
+ALLE_OPTIONEN='${ALLE_OPTIONEN}'
+SONSTIGE_OPTIONEN='${SONSTIGE_OPTIONEN}'
+SCHNITTZEITEN='${SCHNITTZEITEN}'
+SCHNITT_OPTION='${SCHNITT_OPTION}'
+"
+#exit 
 
 #------------------------------------------------------------------------------#
 
 #set -x
 for _E in ${ENDUNG_1} ${ENDUNG_2}
 do
-        echo "${AVERZ}/Filmwandler.sh ${ALLE_OPTIONEN} -q \"${FILMDATEI}\" -z \"${ZIELVERZ}/${ZIELNAME}.${_E}\" ${SCHNITT_OPTION}"
-        ${AVERZ}/Filmwandler.sh ${ALLE_OPTIONEN} -q "${FILMDATEI}" -z "${ZIELVERZ}/${ZIELNAME}.${_E}" ${SCHNITT_OPTION}
+	echo "${AVERZ}/Filmwandler.sh ${SONSTIGE_OPTIONEN} -q \"${FILMDATEI}\" -z \"${ZIELVERZ}/${ZIELNAME}.${_E}\" ${SCHNITT_OPTION}"
+	${AVERZ}/Filmwandler.sh ${SONSTIGE_OPTIONEN} -q "${FILMDATEI}" -z "${ZIELVERZ}/${ZIELNAME}.${_E}" ${SCHNITT_OPTION}
 done
 
 #------------------------------------------------------------------------------#
