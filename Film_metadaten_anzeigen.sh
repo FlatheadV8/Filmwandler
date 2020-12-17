631b58c5324e2fad2c6279071e309aebe33a6493
FlatheadV8 <FlatheadV8@users.noreply.github.com>
2020-12-17 10:20:28 +0100

    Create Film_metadaten_anzeigen.sh
    zeigt die META-Daten zu einem Film an

---
+++ Film_metadaten_anzeigen.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#------------------------------------------------------------------------------#
+#
+# https://ffmpeg.org/ffprobe.html#Main-options
+#
+#------------------------------------------------------------------------------#
+# ffprobe -v error -select_streams v:0 -show_entries stream=width,height,duration,bit_rate -of default=noprint_wrappers=1 input.mp4
+#------------------------------------------------------------------------------#
+
+TITEL="$(ffprobe -v error -probesize 9223372036G -analyzeduration 9223372036G -i "${1}" -show_entries format_tags=title -of compact=p=0:nk=1)"
+
+KOMMENTAR="$(ffprobe -v error -probesize 9223372036G -analyzeduration 9223372036G -i "${1}" -show_entries format_tags=comment -of compact=p=0:nk=1)"
+
+BESCHREIBUNG="$(ffprobe -v error -probesize 9223372036G -analyzeduration 9223372036G -i "${1}" -show_entries format_tags=description -of compact=p=0:nk=1)"
+
+#TITEL_KOMMENTAR="$(ffprobe -v error -probesize 9223372036G -analyzeduration 9223372036G -i "${1}" -show_entries format_tags=title,comment -of compact=p=0:nk=1)"
+
+ID_SPRACHE_AUDIO_SPUREN="$(ffprobe -v error -probesize 9223372036G -analyzeduration 9223372036G -i "${1}" -show_entries stream=index:stream_tags=language -select_streams a -of compact=p=0:nk=1)"
+
+echo "
+TITEL='${TITEL}'
+
+KOMMENTAR='${KOMMENTAR}'
+
+BESCHREIBUNG='${BESCHREIBUNG}'
+
+ID_SPRACHE_AUDIO_SPUREN='${ID_SPRACHE_AUDIO_SPUREN}'
+"
